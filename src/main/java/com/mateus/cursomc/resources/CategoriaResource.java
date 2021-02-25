f3ee3ab2998ad6f5e4dad389c2724292bf0755d9
Mateus Curcino <mateuscurcino@yahoo.com.br>
2021-02-24 23:00:42 -0300

    nova categoria com post

--- src/main/java/com/mateus/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mateus/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.mateus.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mateus.cursomc.domain.Categoria;
 import com.mateus.cursomc.services.CategoriaService;
@@ -35,6 +36,16 @@ public class CategoriaResource {
 		lista.add(cat2);
 		
 		return lista;*/
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
 	}
 	
 }
