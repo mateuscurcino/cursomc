cb8edc2d4d4e0a8cfbd9a8e24020034318ca2389
Mateus Curcino <mateuscurcino@yahoo.com.br>
2021-03-01 23:23:04 -0300

    categoria put e categoria dto

--- src/main/java/com/mateus/cursomc/services/CategoriaService.java
+++ src/main/java/com/mateus/cursomc/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.mateus.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mateus.cursomc.domain.Categoria;
 import com.mateus.cursomc.repositories.CategoriaRepository;
+import com.mateus.cursomc.services.exception.DataIntegrityException;
 import com.mateus.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -26,12 +29,26 @@ public class CategoriaService {
 	}
 	
 	public Categoria insert(Categoria obj) {
-		find(obj.getId());
+		obj.setId(null);
 		return repo.save(obj);
 	}
 	
 	public Categoria update(Categoria obj) {
-		
+		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e ) {
+			throw new DataIntegrityException("Nao é possivel excluir uma categoria que possui produtos");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
